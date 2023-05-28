58a548e1a1ae1523e0bcde8d8f56a8e9dae6563d
Maria Pisarevskaya <mary.may2005@gmail.com>
2023-05-28 23:38:10 +0300

    task6

--- Program.cs
+++ Program.cs
@@ -20,29 +20,36 @@ namespace test1
             //     Console.WriteLine("Число 2 - большее, число 1 - меньшее");
             // }
             // _____________дз 1 Задача 4___________________________________
-            Console.WriteLine("Введите число 1");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите число 2");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите число 3");
-            int num3 = Convert.ToInt32(Console.ReadLine());
-            if (num1 > num2)
+            // Console.WriteLine("Введите число 1");
+            // int num1 = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Введите число 2");
+            // int num2 = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Введите число 3");
+            // int num3 = Convert.ToInt32(Console.ReadLine());
+            // if (num1 > num2)
+            // {
+            //  if (num1 > num3)
+            //  {
+            //     Console.WriteLine($"Число {num1} - большее");
+            //  } else 
+            //     { 
+            //         Console.WriteLine($"Число {num3} - большее");
+            //     }
+            // } else if (num2 > num3)
+            // {
+            //     Console.WriteLine($"Число {num2} - большее");
+            // } else 
+            // {
+            //     Console.WriteLine($"Число {num3} - большее");
+            // }
+            // ____________дз 1 Задача 6___________________________________________
+            Console.WriteLine("Введите число");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num%2 == 0)
             {
-             if (num1 > num3)
-             {
-                Console.WriteLine($"Число {num1} - большее");
-             } else 
-                { 
-                    Console.WriteLine($"Число {num3} - большее");
-                }
-            } else if (num2 > num3)
-            {
-                Console.WriteLine($"Число {num2} - большее");
-            } else 
-            {
-                Console.WriteLine($"Число {num3} - большее");
+                Console.WriteLine($"Число {num} является четным");
             }
-
+            else Console.WriteLine($"Число {num} не является четным");
         }
     }
 }
