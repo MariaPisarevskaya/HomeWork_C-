0b5e1cd3ed22357492cf22bb1aa29d86db0c28b5
Maria Pisarevskaya <mary.may2005@gmail.com>
2023-06-10 17:40:12 +0300

    task 10

--- Program.cs
+++ Program.cs
@@ -7,12 +7,27 @@ namespace dz2
         static void Main(string[] args)
         {
            // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-            // Console.WriteLine("Hello World!");
-            Console.WriteLine("Введите трехзначное число");
-            int num = Convert.ToInt32(Console.ReadLine());
-            int num1 = num % 100 ;
-            int num2 = num1 / 10 ;
-            Console.WriteLine($"Вторая цифра числа = {num2}");
+            // Console.WriteLine("Введите трехзначное число");
+            // int num = Convert.ToInt32(Console.ReadLine());
+            // int num1 = num % 100 ;
+            // int num2 = num1 / 10 ;
+            // Console.WriteLine($"Вторая цифра числа = {num2}");
+        //    Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+               Console.WriteLine("Введите число");
+               int num = Convert.ToInt32(Console.ReadLine());
+               if (num < 100)
+               {
+                Console.WriteLine("В числе нет 3-й цифры");
+               } else
+               {
+                while (num >= 1000)
+                   { 
+                    num = num / 10 ;
+                   }
+                   Console.WriteLine(num % 10);
+               }             
+                    
+
         }
     }
 }
