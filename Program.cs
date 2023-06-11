cadc4d1a9ced544d33ac5af5f7c87f42cae1064e
Maria Pisarevskaya <mary.may2005@gmail.com>
2023-06-11 15:19:02 +0300

    task 19

--- Program.cs
+++ Program.cs
@@ -57,19 +57,32 @@ namespace dz2
 //                Console.WriteLine(d);
 //                ______________Задача 23
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-               Console.WriteLine("Введите число");
-               int num = Convert.ToInt32(Console.ReadLine());
+//                Console.WriteLine("Введите число");
+//                int num = Convert.ToInt32(Console.ReadLine());
 
-               int count = 1;
-               if (num<count)
+//                int count = 1;
+//                if (num<count)
+//                {
+//                 Console.WriteLine("Введите positive число");
+//                }
+//                while (count <= num)
+//                {
+//                 Console.WriteLine(Math.Pow(count,3));
+//                 count++;
+//                }
+//                ____________________________Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+               Console.WriteLine("Введите число");
+               int N = Convert.ToInt32(Console.ReadLine());
+               if ((N / 10000 == N % 10) && ((N/1000) % 10) == ((N / 10) % 10))
                {
-                Console.WriteLine("Введите positive число");
+                 Console.WriteLine("it is palindrom!");
                }
-               while (count <= num)
+               else
                {
-                Console.WriteLine(Math.Pow(count,3));
-                count++;
+                Console.WriteLine("it is NOT palindrom(");
                }
+
         }
     }
 }
